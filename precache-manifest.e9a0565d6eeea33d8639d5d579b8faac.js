e02de1748e590a5dc6baa169680e7a31cb9dd272
Emanuel S. Pedrozo <mr.espedrozo@gmail.com>
2022-06-20 21:54:44 -0300

    Updates

---
+++ precache-manifest.e9a0565d6eeea33d8639d5d579b8faac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "890578f1e18727152684",
+    "url": "/roboFriends/static/css/main.a90f95d2.chunk.css"
+  },
+  {
+    "revision": "890578f1e18727152684",
+    "url": "/roboFriends/static/js/main.890578f1.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/roboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a203aecf8c8d15363f39",
+    "url": "/roboFriends/static/css/2.a055def9.chunk.css"
+  },
+  {
+    "revision": "a203aecf8c8d15363f39",
+    "url": "/roboFriends/static/js/2.a203aecf.chunk.js"
+  },
+  {
+    "revision": "31b5602a7a54ec7a6360",
+    "url": "/roboFriends/static/js/runtime~main.31b5602a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f73120e92824e79b425f72fa16c671f6",
+    "url": "/roboFriends/index.html"
+  }
+];
